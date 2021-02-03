94ef333f3aaec53a567078919ce175423892ce65
Jeredas <efioss93@mail.ru>
2021-02-03 22:45:07 +0300

    fix: fix bug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-module.exports = function towelSort (matrix) {
-  if (!arguments.length) return [];
-  let arr = [];
-  matrix.forEach((x, i) => {
-      if (i % 2 == 0)
-          Array.prototype.push.apply(arr, x);
-      else
-          Array.prototype.push.apply(arr, x.reverse());
-  });
-  return arr;
+module.exports = function towelSort(matrix) {
+
+	if (!matrix || matrix.length == 0) return []; 
+
+	const result = [];
+
+	for (let i = 0; i < matrix.length; i++){
+	
+			matrix[i].sort((a, b) => a - b);}
+
+		matrix[i].forEach((el) => result.push(el));
+	
+
+	return result;
+	
 }
